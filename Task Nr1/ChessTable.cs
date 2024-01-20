e45af443467439cdadab5d008c9c62a64b51a5f8
Egor Busuioc <busuiocegor@gmail.com>
2024-01-20 13:05:01 +0200

    Task Nr1 and Nr2

---
+++ Task Nr1/ChessTable.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task_Nr1;
+
+    //Create a method to determine the color of any chess board cell by its notation:
+    
+    //Example:
+    //Input: a1
+    //Result: Black
+
+class ChessTable
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Write down position");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("You wrote - " + userInput);
+
+            Console.WriteLine("Result: " + DefineColorOfChessTable(userInput));
+        }
+        catch
+        {
+            Console.WriteLine("Exception was trowed! You must input a correct string like 'a8'");
+        }
+    }
+
+    public static string DefineColorOfChessTable(string position)
+    {
+        position = position.ToLower();
+        if (!Regex.IsMatch(position, "^[a-h][1-8]$"))
+        {
+            throw new ArgumentOutOfRangeException(position);
+        }
+
+        if (position[0] + position[1] % 2 == 1)
+        {
+            return "White";
+        }
+        
+        return "Black";
+    }
+}
---
+++ Task Nr2/SubStrings.cs
@@ -0,0 +1,66 @@
+﻿namespace Task_Nr2;
+
+    //Write a program that takes a sequence of characters (string) from the command line as an argument,
+    //and which displays to the console all subsequences (two or more characters) that do not contain
+    //two consecutive repeating characters.
+
+    //Example:
+    //Input: access
+    //Result: ac, ce, ces, es
+
+
+class SubStrings
+{
+    static void Main(string[] args)
+    {
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Program cannot define substrings from empty string");
+                return;
+            }
+
+            string inputString = args[0];
+            List<string> result = GetValidSubsequences(inputString);
+            Console.Write("Substrings: ");
+            
+            foreach (string subString in result)
+            {
+                Console.Write(subString + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        static List<string> GetValidSubsequences(string input)
+        {
+            List<string> subStrings = new List<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = i + 2; j <= input.Length; j++)
+                {
+                    string subString = input.Substring(i, j - i);
+                    if (IsValid(subString))
+                    {
+                        subStrings.Add(subString);
+                    }
+                }
+            }
+
+            return subStrings;
+        }
+
+        static bool IsValid(string subsequence)
+        {
+            for (int i = 0; i < subsequence.Length - 1; i++)
+            {
+                if (subsequence[i] == subsequence[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
